7cc561e411b2c18bbeee5348d125827f98a38391
idunno.aiii@gmail.com <idunno.aiii@gmail.com>
2023-12-17 16:03:18 +0700

    done

--- Program.cs
+++ Program.cs
@@ -1,55 +1,64 @@
-﻿using Dumpify;
-
-var io1 = new ReadIO(x => x * 2)
-    .Map(x => x + 5)
-    .Map(x => x * 7)
-    .Map(x => x * 100);
-
-Console.WriteLine(io1.Run(Console.ReadLine));
+﻿
 
 
-delegate string? Read();
+using Dumpify;
 
-struct ReadIO
-{
-    public Func<int, int> fn;
+// Func<int, string[], (int, string[])> add2 = (int x, string[] log) => (x + 2, [..log, "add2"]);
+// Func<int, string[], (int, string[])> mul3 = (int x, string[] log) => (x * 3, [..log, "mul3"]);
 
-    public ReadIO(Func<int, int> fn)
-    {
-        this.fn = fn;
-    }
+// var r1 = add2(1, []);
+// var r2 = mul3(r1.Item1, r1.Item2);
 
-    public int Run(Read read)
-    {
-        int number = int.Parse(read()!);
-        return fn(number);
-    }
+// r2.Dump();
 
-    public ReadIO Bind(ReadIO next)
-    {
-        var fnCopy = fn;
-        return new(x => next.fn(fnCopy(x)));
-    }
 
-    public ReadIO Map(Func<int, int> next)
-    {
-        var fnCopy = fn;
-        return new((int x) => next(fnCopy(x)));
-    }
-}
 
-struct Function<T>(Func<T, T> fn)
-{
-    public Func<T, T> run = fn;
 
-    public Function<T> Then(Function<T> next)
-    {
-        var runCopy = run;
-        return new Function<T>(x => next.run(runCopy(x)));
-    }
+var add2 = (int x) => new WithLog(x + 2, ["add2"]);
+var mul3 = (int x) => new WithLog(x * 3, ["mul3"]);
 
-    public static Function<T> operator >>(Function<T> left, Function<T> right)
-    {
-        return new Function<T>(x => right.run(left.run(x)));
-    }
-}
+
+var bind = (WithLog w, Func<int, WithLog> fn) => {
+    WithLog newValue = fn(w.Value);
+    return new WithLog(newValue.Value, [..w.Log, ..newValue.Log]);
+};
+
+var r1 = add2(1);
+var r2 = bind(r1, mul3);
+r2.Dump();
+
+record WithLog(int Value, string[] Log);
+
+
+
+
+
+
+// struct WithLog
+// {
+//     public Func<int, int> fn;
+
+//     public WithLog(Func<int, int> fn)
+//     {
+//         this.fn = fn;
+//     }
+
+//     // public int Run(Read read)
+//     // {
+//     //     int number = int.Parse(read()!);
+//     //     return fn(number);
+//     // }
+
+//     public ReadIO Bind(ReadIO next)
+//     {
+//         var fnCopy = fn;
+//         return new(x => next.fn(fnCopy(x)));
+//     }
+
+//     public ReadIO Map(Func<int, int> next)
+//     {
+//         var fnCopy = fn;
+//         return new((int x) => next(fnCopy(x)));
+//     }
+// }
+
